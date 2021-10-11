e9f1530b5a5ffc08ba081f3c97b840ebc4bd426a
ClaudioMandujano <claudio.mandujano@gmail.com>
2021-10-11 20:39:15 -0300

    Agregue parte de tickes

---
+++ tickets.js
@@ -0,0 +1,31 @@
+
+function calcular(){ 
+    let categoria = document.getElementById("categoria").value;
+    console.log(categoria);
+    let descuento;
+
+    if (categoria=="junior" ){
+        descuento = 0.15;
+    }
+    if (categoria=="trainee" ){
+        descuento = 0.50;
+    }
+    if (categoria=="estudiante" ){
+        descuento = 0.80;
+    }
+    let cantidad = document.getElementById('cantidad');
+    let precio = 200;      
+    let total = parseInt(cantidad.value) * precio ;
+    let resultado = document.getElementById('resultado');
+    
+        resultado.innerHTML =  'Total a pagar:  ' + (total - (total * descuento)) +'$';   
+       
+        console.log(resultado);
+}
+
+
+
+const boton = document.getElementById('boton');
+boton.addEventListener('click',calcular);
+
+
